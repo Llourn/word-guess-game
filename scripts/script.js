f90d8e1400e4cfa7f149a74378ccf5dbcbf31076
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2023-02-03 22:45:02 -0400

    added timer and game states

--- scripts/script.js
+++ scripts/script.js
@@ -3,16 +3,16 @@ import { RANDOM_WORD_API_KEY } from "./env.js";
 /*
 The completed application should meet the following criteria:
 
-As a user, I want to start the game by clicking on a button.
-As a user, I want to try and guess a word by filling in a number of blanks that match the number of letters in that word.
-As a user, I want the game to be timed.
+✅ As a user, I want to start the game by clicking on a button.
+✅ As a user, I want to try and guess a word by filling in a number of blanks that match the number of letters in that word.
+✅ As a user, I want the game to be timed.
 As a user, I want to win the game when I have guessed all the letters in the word.
 As a user, I want to lose the game when the timer runs out before I have guessed all the letters.
 As a user, I want to see my total wins and losses on the screen.
 
 Specifications
-When a user presses a letter key, the user's guess should be captured as a key event.
-When a user correctly guesses a letter, the corresponding blank "_" should be replaced by the letter. For example, if the user correctly selects "a", then "a _ _ a _" should appear.
+✅ When a user presses a letter key, the user's guess should be captured as a key event.
+✅ When a user correctly guesses a letter, the corresponding blank "_" should be replaced by the letter. For example, if the user correctly selects "a", then "a _ _ a _" should appear.
 When a user wins or loses a game, a message should appear and the timer should stop.
 When a user clicks the start button, the timer should reset.
 When a user refreshes or returns to the brower page, the win and loss counts should persist.
@@ -20,6 +20,9 @@ When a user refreshes or returns to the brower page, the win and loss counts sho
 
 var wordSpan = document.querySelector("#word-to-guess");
 var startButton = document.querySelector("#start-button");
+var timerSpan = document.querySelector("#timer");
+var gameIsActive = false;
+var timerInterval;
 var currentWordToGuess = "";
 var blankedWord = [];
 
@@ -40,21 +43,34 @@ async function getRandomWord(type = "") {
   );
 
   var data = await response.json();
-  return data.word;
+  console.log(data.word);
+  return data.word.toLowerCase();
 }
 
 async function newWord() {
   wordSpan.textContent = "⚙️ loading new word...";
 
-  /* currentWordToGuess = await getRandomWord().then((response) => {
-    blankedWord = replaceWithUnderscores(response);
-    return response;
-  });
-  updateWordSpan(); */
-
   currentWordToGuess = await getRandomWord();
   blankedWord = [...replaceWithUnderscores(currentWordToGuess)];
+  startTimer(5);
   updateWordSpan();
+  gameIsActive = true;
+}
+
+function startTimer(timeInSeconds) {
+  var countDown = timeInSeconds;
+  timerSpan.textContent = `${countDown}s`;
+
+  timerInterval = setInterval(function () {
+    countDown--;
+    timerSpan.textContent = `${countDown}s`;
+    if (countDown < 1) {
+      clearInterval(timerInterval);
+      timerSpan.textContent = "Game over";
+      showStartButton();
+      gameIsActive = false;
+    }
+  }, 1000);
 }
 
 function replaceWithUnderscores(word) {
@@ -69,19 +85,38 @@ function updateWordSpan() {
   wordSpan.textContent = blankedWord.join("");
 }
 
+function checkSuccess() {
+  return !blankedWord.includes("_");
+}
+
+function showStartButton() {
+  startButton.setAttribute("style", "display: unset;");
+}
+
+function hideStartButton() {
+  startButton.setAttribute("style", "display: none;");
+}
+
 window.addEventListener("keydown", function (event) {
-  var keypressed = event.key;
-  for (let i = 0; i < currentWordToGuess.length; i++) {
-    const letter = currentWordToGuess[i];
-    if (letter === keypressed) {
-      blankedWord[i] = letter;
-    }
-    if (blankedWord != wordSpan.textContent) {
-      updateWordSpan();
+  if (gameIsActive) {
+    var keypressed = event.key;
+    for (let i = 0; i < currentWordToGuess.length; i++) {
+      const letter = currentWordToGuess[i];
+      if (letter === keypressed) {
+        blankedWord[i] = letter;
+      }
+      if (blankedWord != wordSpan.textContent) {
+        updateWordSpan();
+      }
+      if (checkSuccess()) {
+        clearInterval(timerInterval);
+        showStartButton();
+      }
     }
   }
 });
 
 startButton.addEventListener("click", function () {
+  hideStartButton();
   newWord();
 });
